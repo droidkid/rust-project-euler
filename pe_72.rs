d36d92ef4099a33f8807e2c70ffdd435197b42ec
Yuvaraj Chesetti <karch08@gmail.com>
2018-09-28 19:29:38 +0530

    Problem 72

--- pe_72.rs
+++ pe_72.rs
@@ -13,7 +13,7 @@ impl NumInfo {
 }
 
 fn calc_factors_for_combo(num_info: &NumInfo, bitmask: i32) -> i32{
-    let mut combo_value = 1;
+    let mut combo_value = -1;
     for i in 0..num_info.prime_factors.len() {
         if (bitmask & (1 << i)) != 0 {
             combo_value = -1 * combo_value * num_info.prime_factors[i];
@@ -27,7 +27,7 @@ fn calc_common_div(num_info:&NumInfo) -> i32 {
     let mut result = 0;
     let total_combinations = 1 << (num_info.prime_factors.len());
 
-    for bitmask in 0..total_combinations {
+    for bitmask in 1..total_combinations {
         result = result + calc_factors_for_combo(&num_info, bitmask);
     }
     result
@@ -36,7 +36,7 @@ fn calc_common_div(num_info:&NumInfo) -> i32 {
 fn main() {
 
     let mut num_infos : Vec<NumInfo> = Vec::new();
-    let lim = 1_000_000usize;
+    let lim = 8usize;
     for i in 0..=lim {
         num_infos.push(NumInfo::new(i as i32));
     }
@@ -57,5 +57,7 @@ fn main() {
     for i in 2usize..=lim {
         result = result + (calc_common_div(&num_infos[i]) as i64);
     }
+
+    println!("{}", result);
 
 }
